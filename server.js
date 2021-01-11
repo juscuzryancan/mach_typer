645d7c8e81130fb19ff3b5cbc3dab22164ab3931
Ryan Riley Puzon <rrgpuzon@gmail.com>
2021-01-10 23:42:36 -0800

    adds client for db

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const server = express();
+
+const client = require('./db/client');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 server.use(morgan('dev'));
+server.use(bodyParser.json());
 
-server.use('/api', require('./routes'));
+server.use('/api', require('./api'));
+
+server.use((err, req, res, next) => {
+    console.error('Error Handler was trigerred ->\n', err);
+    res.setStatus(500).send(err);
+});
 
 server.listen(5000, () => {
-    console.log('connected on 5000');
+    client.connect();
+    console.log('The server is now up');
 })
