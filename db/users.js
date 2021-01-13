bde8f141eced0fdf681e3f8692b7f713aea138fe
Ryan Riley Puzon <rrgpuzon@gmail.com>
2021-01-12 21:58:59 -0800

    creates user db adapters

---
+++ db/users.js
@@ -0,0 +1,43 @@
+const client = require('./client');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const createUser = async ({username, password}) => {
+    try {
+        const hash = await bcrypt.hash(password, saltRounds);
+        const { rows: [user] } = await client.query(`
+        INSERT INTO users (username, password)
+        VALUES ($1, $2)
+        RETURNING *;
+        `, [username, hash]);
+        delete user.password;
+        return user;
+    } catch (error) {
+        console.error('createUser(): Failed creating user');
+        console.error(error);
+    }
+}
+
+const getUser = async({ username, password }) => {
+    try {
+        const { rows: [user] } = await client.query(`
+            SELECT * FROM users
+            WHERE username = $1;
+        `, [username]);
+        if (user) {
+            const match = await bcrypt.compare(password, user.password);
+            if (match) {
+                delete user.password;
+                return user;
+            }
+        }
+    } catch (error) {
+        console.error('getUser(): Failed getting user');
+        console.error(error);
+    }
+}
+
+module.exports = {
+    createUser,
+    getUser,
+}
